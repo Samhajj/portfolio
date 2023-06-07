73131b0a0b91a39c865d6910cdf4447b38ac9429
Ajibade Samson <94762634+Samhajj@users.noreply.github.com>
2023-06-07 17:13:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,129 @@
+`use strict`;
+
+// Menu items
+const navItem1 = document.querySelector(".active");
+const navItem2 = document.querySelector(".about");
+const navItem3 = document.querySelector(".work");
+const navItem4 = document.querySelector(".blog");
+const navItem5 = document.querySelector(".testimonials");
+const navItem6 = document.querySelector(".contact");
+const navItems = [navItem1, navItem2, navItem3, navItem4, navItem5, navItem6];
+const mobileMenuToggle = document.querySelector(".fa-bars");
+const mobileMenu = document.querySelector(".mobile-menu-overlay");
+const clearMenu = document.querySelector(".fa-times");
+let [a, ...others] = [...navItems];
+
+// let counter = 0;
+// for (let i = 0; i < navItems.length; i++) {
+//   navItems[i].addEventListener("click", function () {
+//     navItems[i].classList.add("active");
+//     navItems[i].classList.remove("active");
+//   });
+// }
+let menuCounter = 0;
+mobileMenuToggle.addEventListener("click", function () {
+  if (menuCounter === 0) {
+    mobileMenu.classList.add("revealed");
+    menuCounter++;
+  } else if (menuCounter === 1) {
+    mobileMenu.classList.remove("revealed");
+    menuCounter = 0;
+  }
+});
+
+clearMenu.addEventListener("click", function () {
+  mobileMenu.classList.remove("revealed");
+});
+
+const menuHoverFunction = function () {
+  a.addEventListener("mouseenter", function () {
+    navItems[1].classList.add("hover");
+    navItems[2].classList.add("hover");
+    navItems[3].classList.add("hover");
+    navItems[4].classList.add("hover");
+    navItems[5].classList.add("hover");
+  });
+  navItem1.addEventListener("mouseout", function () {
+    navItems[1].classList.remove("hover");
+    navItems[2].classList.remove("hover");
+    navItems[3].classList.remove("hover");
+    navItems[4].classList.remove("hover");
+    navItems[5].classList.remove("hover");
+  });
+
+  navItem2.addEventListener("mouseenter", function () {
+    navItems[0].classList.add("hover");
+    navItems[2].classList.add("hover");
+    navItems[3].classList.add("hover");
+    navItems[4].classList.add("hover");
+    navItems[5].classList.add("hover");
+  });
+  navItem2.addEventListener("mouseout", function () {
+    navItems[0].classList.remove("hover");
+    navItems[2].classList.remove("hover");
+    navItems[3].classList.remove("hover");
+    navItems[4].classList.remove("hover");
+    navItems[5].classList.remove("hover");
+  });
+
+  navItem3.addEventListener("mouseenter", function () {
+    navItems[0].classList.add("hover");
+    navItems[1].classList.add("hover");
+    navItems[3].classList.add("hover");
+    navItems[4].classList.add("hover");
+    navItems[5].classList.add("hover");
+  });
+  navItem3.addEventListener("mouseout", function () {
+    navItems[0].classList.remove("hover");
+    navItems[1].classList.remove("hover");
+    navItems[3].classList.remove("hover");
+    navItems[4].classList.remove("hover");
+    navItems[5].classList.remove("hover");
+  });
+
+  navItem4.addEventListener("mouseenter", function () {
+    navItems[0].classList.add("hover");
+    navItems[1].classList.add("hover");
+    navItems[2].classList.add("hover");
+    navItems[4].classList.add("hover");
+    navItems[5].classList.add("hover");
+  });
+  navItem4.addEventListener("mouseout", function () {
+    navItems[0].classList.remove("hover");
+    navItems[1].classList.remove("hover");
+    navItems[2].classList.remove("hover");
+    navItems[4].classList.remove("hover");
+    navItems[5].classList.remove("hover");
+  });
+
+  navItem5.addEventListener("mouseenter", function () {
+    navItems[0].classList.add("hover");
+    navItems[1].classList.add("hover");
+    navItems[2].classList.add("hover");
+    navItems[3].classList.add("hover");
+    navItems[5].classList.add("hover");
+  });
+  navItem5.addEventListener("mouseout", function () {
+    navItems[0].classList.remove("hover");
+    navItems[1].classList.remove("hover");
+    navItems[2].classList.remove("hover");
+    navItems[3].classList.remove("hover");
+    navItems[5].classList.remove("hover");
+  });
+
+  navItem6.addEventListener("mouseenter", function () {
+    navItems[0].classList.add("hover");
+    navItems[1].classList.add("hover");
+    navItems[2].classList.add("hover");
+    navItems[3].classList.add("hover");
+    navItems[4].classList.add("hover");
+  });
+  navItem6.addEventListener("mouseout", function () {
+    navItems[0].classList.remove("hover");
+    navItems[1].classList.remove("hover");
+    navItems[2].classList.remove("hover");
+    navItems[3].classList.remove("hover");
+    navItems[4].classList.remove("hover");
+  });
+};
+menuHoverFunction();
